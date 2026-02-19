62af63d0d38e22980bc3b164226675b2dfa7ae1b
alexandre <alexandre020602@gmail.com>
2026-02-19 17:36:12 -0300

    clear

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="bg-blue-500">
-      <div className="text-red-500">Hello world!</div>
+    <main className="">
+      <div className="">Hello world!</div>
     </main>
   );
 }
