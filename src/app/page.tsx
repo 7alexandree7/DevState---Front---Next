6e5438e83430dac188eb9fa188e3bd9b2b297b27
alexandre <alexandre020602@gmail.com>
2026-02-20 20:59:08 -0300

    clear page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
-import { Button } from "@/components/Button/Button";
-import { IconButton } from "@/components/IconButton/IconButton";
-import { ArrowRight, Copy } from "lucide-react";
+
 
 export default function Home() {
   return (
     <main className="">
-
-
-      <Button type="button">
-        Enviar
-        <ArrowRight className="" />
-      </Button>
-
-
-      <IconButton type="button">
-        <Copy className="" />
-      </IconButton>
-
-
+      next process - Building landing page
     </main>
   );
 }
