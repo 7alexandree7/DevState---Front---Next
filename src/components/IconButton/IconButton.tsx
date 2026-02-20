79ae0968e300b5b555659fac491357e3e2d0be7b
alexandre <alexandre020602@gmail.com>
2026-02-20 11:48:19 -0300

    sintaxe error

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -1,13 +1,14 @@
 import { ComponentProps } from "react";
 
-interface IconButtonProps extends ComponentProps<"button"> {}
+interface IconButtonProps extends ComponentProps<"button"> { }
 
 
 export function IconButton(props: IconButtonProps) {
 
     return (
         <button
-            className=" bg-gray-500 text-blue p-1.5 rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300">
-        </button>
+            className=" bg-gray-500 text-blue p-1.5 rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
+            {...props}
+        />
     )
 }
