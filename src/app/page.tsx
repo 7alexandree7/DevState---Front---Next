f7f178feac5c9495515bc817f1bea7e1ec22b9c3
alexandre <alexandre020602@gmail.com>
2026-02-20 11:22:46 -0300

    First button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/Button/Button";
+import { ArrowRight } from "lucide-react";
+
 export default function Home() {
   return (
     <main className="">
-      <div className="">Hello world!</div>
+      <Button>
+        Enviar
+        <ArrowRight className="w-4 h-4 ml-2" />
+      </Button>
     </main>
   );
 }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+interface ButtonProps {
+    children?: React.ReactNode;
+}
+
+
+export function Button({ children }: ButtonProps) {
+
+    return (
+        <button
+            className=" flex justify-between items-center bg-gray-500 text-blue px-5 h-12 font-semibold rounded-xl w-full cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300">
+            {children}
+        </button>
+    )
+}
