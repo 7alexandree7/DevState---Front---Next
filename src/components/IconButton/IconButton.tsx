a1addd2c267fa75496812bd428cdf7d6f6f4e85a
alexandre <alexandre020602@gmail.com>
2026-02-23 11:26:38 -0300

    twmerge

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -1,13 +1,14 @@
 import { type ComponentProps } from "react";
+import { twMerge } from "tailwind-merge"
 
 interface IconButtonProps extends ComponentProps<"button"> { }
 
 
-export function IconButton(props: IconButtonProps) {
+export function IconButton({ className, ...props }: IconButtonProps) {
 
     return (
         <button
-            className=" bg-gray-500 text-blue p-1.5 rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
+            className={twMerge("bg-gray-500 text-blue p-1.5 rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300", className)}
             {...props}
         />
     )
