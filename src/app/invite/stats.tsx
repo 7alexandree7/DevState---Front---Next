bb768d4efba1c9c15821f226dfb69f3fbae84abe
alexandre <alexandre020602@gmail.com>
2026-02-23 16:49:33 -0300

    Stats Component UI

---
+++ src/app/invite/stats.tsx
@@ -0,0 +1,23 @@
+import { BadgeCheck, Medal, MousePointerClick } from 'lucide-react'
+
+export default function Stats() {
+    return (
+        <div className='grid gap-3 md:grid-cols-3'>
+            <div className=' bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl relative'>
+                <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>1042</span>
+                <span className='text-sm text-gray-300 text-center leading-none'>Acessos ao link</span>
+                <MousePointerClick className='size5 text-purple absolute top-3 left-3' />
+            </div>
+            <div className=' bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl relative'>
+                <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>1042</span>
+                <span className='text-sm text-gray-300 text-center leading-none'>Inscrições feitas</span>
+                <BadgeCheck className='size5 text-purple absolute top-3 left-3' />
+            </div>
+            <div className=' bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl relative'>
+                <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>3°</span>
+                <span className='text-sm text-gray-300 text-center leading-none'>Posição no ranking</span>
+                <Medal className='size5 text-purple absolute top-3 left-3' />
+            </div>
+        </div>
+    )
+}
